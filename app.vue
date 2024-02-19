8d12040f40385b24c0f453c49f9bc61707b1a8d4
danielhill1982 <mail@dhill.dev>
2024-02-19 15:07:54 +0100

    bla

--- app.vue
+++ app.vue
@@ -1,28 +1,25 @@
 <template>
   <div style="max-width:600px; margin-left:auto; margin-right: auto;">
     <header>
-      <p>dhill.dev</p>
-      <p>- - - -</p>
-      <p><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="14" height="14">
+      <p>dhill.dev <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="18" height="18">
   <path stroke-linecap="round" stroke-linejoin="round" d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25" />
 </svg> Intuitive Web Solutions</p>
+      <p>- - - -</p>
       <p><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-          width="14" height="14">
+          width="18" height="18">
           <path stroke-linecap="round" stroke-linejoin="round"
             d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
         </svg>
-        Consulting and Conception <br><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-          strokeWidth={1.5} stroke="currentColor" width="14" height="14">
+        Consulting and Conception</p><p><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+          strokeWidth={1.5} stroke="currentColor" width="18" height="18">
           <path strokeLinecap="round" strokeLinejoin="round"
             d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672Zm-7.518-.267A8.25 8.25 0 1 1 20.25 10.5M8.288 14.212A5.25 5.25 0 1 1 17.25 10.5" />
         </svg>
-        Logo and UI/UX Design <br><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-          stroke-width="1.5" stroke="currentColor" width="14" height="14">
+        Logo and UI/UX Design</p><p><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+          stroke-width="1.5" stroke="currentColor" width="18" height="18">
           <path stroke-linecap="round" stroke-linejoin="round"
             d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
-        </svg>
-
-        Software Development</p>
+        </svg> Software Development</p>
 
       <div class="stripes" style="width:300px; height: 100px;">
 
@@ -36,7 +33,7 @@
     </main>
     <footer>
       <div style="text-align: right;">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="14" height="14">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="18" height="18">
   <path fill-rule="evenodd" d="M7.793 2.232a.75.75 0 0 1-.025 1.06L3.622 7.25h10.003a5.375 5.375 0 0 1 0 10.75H10.75a.75.75 0 0 1 0-1.5h2.875a3.875 3.875 0 0 0 0-7.75H3.622l4.146 3.957a.75.75 0 0 1-1.036 1.085l-5.5-5.25a.75.75 0 0 1 0-1.085l5.5-5.25a.75.75 0 0 1 1.06.025Z" clip-rule="evenodd" />
 </svg> <NuxtLink to="/">Back</NuxtLink>
 </div>
@@ -47,14 +44,14 @@
         <img src="/danielhill.jpg" width="92px" height="92px" alt="" title="" style="border-radius: 9999px;" />
       </div>
       
-      <p><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="14" height="14">
+      <p><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="18" height="18">
   <path stroke-linecap="round" stroke-linejoin="round" d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3" />
 </svg> If you are interested or just have questions, send me an email to <a href="mailto:mail@dhill.dev"
           title="Write An Email">mail@dhill.dev</a></p>
 
       <p>- - - -</p>
       <p>Copyright © 2024 - dhill.dev</p>
-      <p><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="14" height="14">
+      <p><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="18" height="18">
   <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z" />
 </svg> Made with <NuxtLink to="https://nuxt.com" target="_blank">Nuxt</NuxtLink>
       </p>
@@ -66,12 +63,12 @@
 <style>
 body {
   font-family:  monospace, 'Courier New', Courier;
-  background-color: floralwhite;
-  color: red;
+  background-color: #EBFFE6;
+  color: #6EC332;
 }
 
 a {
-  color: red;
+  color: #6EC332;
 }
 
 a:hover {
@@ -94,7 +91,7 @@ a:hover {
   margin-bottom: 25px;
   --b: 1px;
   /* thickness of the border */
-  --c: red;
+  --c: #6EC332;
   /* color of the border */
   --w: 20px;
   /* width of border */
@@ -112,8 +109,8 @@ a:hover {
     repeating-linear-gradient(-45deg,
       transparent,
       transparent 0.5rem,
-      red 0.5rem,
-      red 1rem);
+      #6EC332 0.5rem,
+      #6EC332 1rem);
   background-size: 400% 400%;
   animation: barberpole 20s linear infinite;
 }
