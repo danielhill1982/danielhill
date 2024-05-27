163e03a0481f26533b80bbe02282b517adf5f78f
danielhill1982 <mail@dhill.dev>
2024-05-27 02:02:10 +0200

    final

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: [
     '@nuxt/content',
     'nuxt-particles'
   ],
+
   content: {
     markdown: {
       anchorLinks: false
     }
   },
+
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
-  }
-
+  },
   
+  build: {
+    transpile: ["gsap"]
+  }  
 })
